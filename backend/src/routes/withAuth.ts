d96db812ac614b00554dd950c1f2faa27d644747
Saulo-Felipe <saulofelipe234567@gmail.com>
2022-05-27 15:18:43 +0000

    adding backend and database

--- backend/src/routes/withAuth.ts
+++ backend/src/routes/withAuth.ts
@@ -7,10 +7,21 @@ export const withAuth = Router();
 
 withAuth.use(MiddlewareAuthentication);
 
+type CreateCategoryBody = {
+  categoryID: string;
+  emojiID: string;
+  content: string;
+  bgColor: string;
+  textColor: string;
+}
+
 
 withAuth.post("/get-user", async (request: Request, response: Response) => {
   try {
     const currentUserId = request.headers.currentUserId;
+
+    if (!currentUserId)
+      throw "Erro de validação";
   
     const [user]: any = await sequelize.query(`
       SELECT name FROM "User" 
@@ -25,18 +36,13 @@ withAuth.post("/get-user", async (request: Request, response: Response) => {
   }
 });
 
-type CreateCategoryBody = {
-  categoryID: string;
-  emojiID: string;
-  content: string;
-  bgColor: string;
-  textColor: string;
-}
-
 withAuth.post("/create-category", async (request: Request, response: Response) => {
   try {
     const { bgColor, content, emojiID, textColor }: CreateCategoryBody = request.body;
     const userID = request.headers.currentUserId;
+
+    if (!bgColor || !content || !emojiID || !textColor || !userID)
+      throw "Erro de validação";
 
     await sequelize.query(`
       INSERT INTO "Categories" (name, "backgroundColor", "contentColor", "iconId", "PkUserId")
@@ -56,9 +62,14 @@ withAuth.post("/get-categories", async(request: Request, response: Response) => 
   try {
     const userId = request.headers.currentUserId;
 
+    if (!userId)
+      throw "Erro de validação";
+
+
     const [categories] = await sequelize.query(`
       SELECT * FROM "Categories" 
       WHERE "PkUserId" = ${userId}
+      ORDER BY id DESC
     `);
 
     return response.json({
@@ -71,5 +82,99 @@ withAuth.post("/get-categories", async(request: Request, response: Response) => 
   catch(e) {
     console.warn("Erro interno!: ", e);
     return response.json({ success: false, error: true, message: "Ocorreu um erro desconhecido ao atualizar categorias." });
+  }
+});
+
+withAuth.post("/get-category", async (request: Request, response: Response) => {
+  try {
+    const { categoryID } = request.body;
+    const currentUserId = request.headers.currentUserId;
+
+    if (!categoryID)
+      throw "Erro de validação"
+    
+    let [result] = await sequelize.query(`
+      SELECT * FROM "Categories"
+      WHERE id = ${ categoryID }
+      AND "PkUserId" = ${ currentUserId }
+    `);
+
+    if (result.length === 0)
+      return response.json({ success: false, error: true, message: "Categoria inválida" });
+    
+    return response.json({ success: true, category: result[0], message: "Tasks carregadas!" });
+
+  }
+  catch(e) {
+    console.warn("Erro interno!: ", e);
+    return response.json({ success: false, error: true, message: "Ocorreu um erro desconhecido ao Carregar tasks." });
+  }
+});
+
+withAuth.post("/delete-category", async (request: Request, response: Response) => {
+  try {
+    const { categoryId } = request.body;
+
+    await sequelize.query(`
+      DELETE FROM "Categories" 
+      WHERE id = ${categoryId}
+    `);
+
+    return response.json({ success: true, error: false, message: "Categoria removida com sucesso!" });
+  }
+  catch(e) {
+    console.warn("Erro interno!: ", e);
+    return response.json({ success: false, error: true, message: "Ocorreu um erro desconhecido ao deletar categoria." });
+  }
+});
+
+withAuth.post("/get-tasks", async (request: Request, response: Response) => {
+  try {
+    const { categoryID } = request.body;
+
+    const [result] = await sequelize.query(`
+      SELECT * FROM "Tasks"
+      WHERE "categoryID" = ${ categoryID }
+    `);
+
+    return response.json({ success: true, error: false, message: "Tasks carregadas com sucesso!", tasks: result });
+  }
+  catch(e) {
+    console.warn("Erro interno!: ", e);
+    return response.json({ success: false, error: true, message: "Ocorreu um erro desconhecido ao carregar tasks." });
+  }
+});
+
+withAuth.post("/create-task", async (request: Request, response: Response) => {
+  try {
+    const { content, categoryID } = request.body;
+
+    await sequelize.query(`
+      INSERT INTO "Tasks" (content, finished, "categoryID")
+      VALUES ('${content}', ${false}, ${categoryID})
+    `);
+
+    return response.json({ success: true, error: false, message: "Task criada com sucesso!" });
+  }
+  catch(e) {
+    console.warn("Erro interno!: ", e);
+    return response.json({ success: false, error: true, message: "Ocorreu um erro desconhecido ao Carregar categoria." });
+  }
+});
+
+withAuth.post("/delete-task", async (request: Request, response: Response) => {
+  try {
+
+    const { taskId } = request.body;
+
+    await sequelize.query(`
+      DELETE FROM 
+    `);
+
+
+  }
+  catch(e) {
+    console.warn("Erro interno!: ", e);
+    return response.json({ success: false, error: true, message: "Ocorreu um erro desconhecido ao Carregar categoria." });
   }
 });
